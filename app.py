d0d92a3e008cf9751a3b16f9406014bca6a2862b
Copheles <xixxykitkit@gmail.com>
2022-01-11 10:40:06 +0630

    fixed

--- app.py
+++ app.py
@@ -24,6 +24,4 @@ api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == "__main__":
-    from db import db
-    db.init_app(app)
     app.run(debug=True)
